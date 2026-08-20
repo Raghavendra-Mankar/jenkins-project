3ab44a2653212edeae6368008e3331cdb82904d1
raghu <mankarraghu123@gmail.com>
2026-08-20 13:41:28 +0530

    Server.js Updated

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log("Server---");
+const http = require('http');
+
+const routes = require("express");
+
+
+const router = routes.Router;
+
+const port = 3000;
+
+// Create server
+const server = http.createServer((req, res) => {
+  res.statusCode = 200; // OK
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+// Start server
+server.listen(port, () => {
+  console.log(`Server running`);
+});
